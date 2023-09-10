ed94363e8de68acfc3e10df0ce6bfbce57dfe0a3
Andreas Bolstad <26123972+andreasbolstad@users.noreply.github.com>
2023-09-10 21:21:18 +0200

    complete bash luhn

--- bash/luhn/luhn.sh
+++ bash/luhn/luhn.sh
@@ -1,24 +1,40 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+main () {
+    n=$1
+    if [[ "${n//[[:digit:] ]/}" ]]; then
+        echo "false"
+        exit 0
+    fi
+    n=${n//[^[:digit:]]/}
+    if [[ ${#n} -lt 2 ]]; then
+        echo "false"
+        exit 0
+    fi
+    l="${#n}"
+    m=""
+    for i in $(echo "$n" | grep -o .); do
+        (( l=l-1))
+        if (( l % 2 == 0 )); then
+            m="${m}${i}"
+        else
+            k=$((i*2))
+            if (( k > 9 )); then
+                m="$m$((k-9))"
+            else
+                m="$m$k"
+            fi
+        fi
+    done
+    declare -i sum=0
+    for j in $(echo "$m" | grep -o .); do
+        ((sum+=j))
+    done
+    if ((sum % 10 == 0)); then
+        echo "true"
+    else
+        echo "false"
+    fi
+}
+
+main "$@"

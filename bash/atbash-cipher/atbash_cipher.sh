e93c4275d3ae51a032e13fce12521902110d84cc
Andreas Bolstad <26123972+andreasbolstad@users.noreply.github.com>
2023-09-16 21:11:14 +0200

    solve bash atbash cipher

--- bash/atbash-cipher/atbash_cipher.sh
+++ bash/atbash-cipher/atbash_cipher.sh
@@ -1,24 +1,53 @@
-#!/usr/bin/env bash
+az=({a..z})
+za=({z..a})
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+main() {
+    mode=$1
+    word=$2
+    if [[ $mode == "encode" ]]; then
+        encode "$word"
+    elif [[ $mode == "decode" ]]; then
+        decode "$word"
+    else
+        echo "Arg #1 must be either encode or decode"
+        exit 1
+    fi
+}
+
+encode() {
+    _word=${1,,}
+    word=${_word//[^[:alnum:]]/}
+    res1=$(flip "$word")
+    res2=""
+    for (( i=0; i<${#res1}; i+=5 ))
+    do
+        res2+="${res1:i:5} "
+    done
+    echo "${res2:0:-1}"
+}
+
+decode() {
+    word=${1//[[:space:]]/}
+    flip "$word"
+}
+
+flip() {
+    word=$1
+    res=""
+    for ((i=0; i<${#word}; i++))
+    do
+        l=${word:i:1}
+        for ((j = 0; j < ${#az[@]}; j++))
+        do
+            if [[ $l == "${az[$j]}" ]]; then
+                res+="${za[$j]}"
+            fi
+        done
+        if [[ $l =~ [1-9] ]]; then
+            res+=$l
+        fi
+    done
+    echo "$res"
+}
+
+main "$@"
